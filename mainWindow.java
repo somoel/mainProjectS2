0d63c8ae75aba11e307bfc2d05dd07f60193e569
somoel <jhamirsami05@gmail.com>
2023-09-04 14:33:51 -0500

    Commit Inicial: No hay nada hecho aún, ni siquiera sé si se puede ejecutar.

---
+++ mainWindow.java
@@ -0,0 +1,13 @@
+import java.awt.event.*;
+import javax.swing.*;
+
+public class mainProjectS2 extends Jframe implements ActionListener{
+    public mainProjectS2(){
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Programa Principal");
+    }
+    public static void main(String[] args) {
+
+    }
+}
