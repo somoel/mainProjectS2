367050b5324fe5c3afe2aec7f27373b8221d2c49
somoel <jhamirsami05@gmail.com>
2023-09-09 09:25:46 -0500

    Versión 1.1.5: Se arregló la interfaz de la ventana de Fibonacci.

--- src/main/java/FiboGUI.java
+++ src/main/java/FiboGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
@@ -23,37 +24,41 @@ public class FiboGUI extends JFrame implements ActionListener {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Programa Fibonacci");
 
-        titleLabel = new JLabel("Números de Fibonacci", SwingConstants.RIGHT);
-        titleLabel.setBounds(10, 10, 400, 30);
+        titleLabel = new JLabel("Fibonacci", SwingConstants.RIGHT);
+        titleLabel.setBounds(10, 10, 400, 50);
         add(titleLabel);
 
-        nLabel = new JLabel("<html>Ingrese la cantidad de números de la serie<br>" +
-                "de Fibonacci que desea obtener</html>");
-        nLabel.setBounds(10, 40, 400, 30);
+        nLabel = new JLabel("Ingrese la cantidad de números de la serie de Fibonacci");
+        nLabel.setBounds(10, 100, 400, 50);
         add(nLabel);
 
         nField = new JTextField();
-        nField.setBounds(10, 70, 400, 30);
+        nField.setHorizontalAlignment(JTextField.CENTER);
+        nField.setBounds(115, 150, 70, 50);
+        nField.addActionListener(this);
         add(nField);
 
         calcFiboButton = new JButton("Calcular");
-        calcFiboButton.setBounds(10, 100, 400, 30);
+        calcFiboButton.setBounds(195, 150, 120, 50);
         calcFiboButton.addActionListener(this);
         add(calcFiboButton);
 
-        resultLabel = new JLabel("");
-        resultLabel.setBounds(10, 130, 400, 30);
+        resultLabel = new JLabel("Aquí debería aparecer el resultado");
+        resultLabel.setVerticalAlignment(resultLabel.TOP);
+        resultLabel.setBounds(10, 210, 400, 210);
         add(resultLabel);
 
         backButton = new JButton("Volver");
-        backButton.setBounds(10, 150, 195, 30);
+        backButton.setBounds(10, 430, 195, 30);
         add(backButton);
 
         closeButton = new JButton("Cerrar");
-        closeButton.setBounds(215, 150, 195, 30);
+        closeButton.setBounds(215, 430, 195, 30);
         add(closeButton);
 
         new Styles(this, titleLabel, nField); // Agrega colores
+
+        nField.setFont(Styles.mainFont.deriveFont(Font.PLAIN, 25));
 
         new BackAndCloseB(this, this.backFrame, backButton, closeButton, null); // Funciones de volver y cerrar
 
@@ -62,7 +67,7 @@ public class FiboGUI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent event) {
 
         // Calculo y muestreo de la serie de Fibonacci
-        if (event.getSource() == calcFiboButton) {
+        if (event.getSource() == calcFiboButton || event.getSource() == nField) {
             try {
                 n = Integer.parseInt(nField.getText()); // Obtener el número
             } catch (Exception NumberFormatException) {
@@ -72,16 +77,17 @@ public class FiboGUI extends JFrame implements ActionListener {
             if (n < 0) { // Revisión de número positivo
                 JOptionPane.showMessageDialog(this, "Error\n\nDebe ser un número positivo.");
             }
+            nField.setText("");
 
             // Devolución del arreglo con los números de la secuencia
             switch (n) {
-                case 0 -> resultLabel.setText("");
-                case 1 -> resultLabel.setText("0");
-                case 2 -> resultLabel.setText("[0, 1]");
+                case 0 -> resultLabel.setText("Resultado: Ningún número.");
+                case 1 -> resultLabel.setText("Resultado: 0");
+                case 2 -> resultLabel.setText("Resultado: 0, 1");
                 default -> {
 
                     // se crea un arreglo del tamaño que el usuario decida
-                    int[] arreglo = new int[n];
+                    long[] arreglo = new long[n];
                     arreglo[0] = 0;
                     arreglo[1] = 1;
 
@@ -94,7 +100,9 @@ public class FiboGUI extends JFrame implements ActionListener {
 
 
                     // impresion de la secuencia fiboonacci
-                    resultLabel.setText(Arrays.toString(arreglo));
+                    resultLabel.setText("<html>Resultado: " +
+                            Arrays.toString(arreglo).substring(1, Arrays.toString(arreglo).length() - 1) +
+                            "</html>");
                 }
             }
 
