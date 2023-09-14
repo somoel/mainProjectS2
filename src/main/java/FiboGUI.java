0b50241b6e0b909e1743cad53b5eb81e34aa6bf9
somoel <jhamirsami05@gmail.com>
2023-09-14 11:38:22 -0500

    Versión 1.4: Programa completo.

--- src/main/java/FiboGUI.java
+++ src/main/java/FiboGUI.java
@@ -8,11 +8,15 @@ import java.util.Arrays;
 public class FiboGUI extends JFrame implements ActionListener {
 
     // Elementos de la ventana fibonacci
-    private JLabel titleLabel, nLabel, resultLabel;
+    private JLabel titleLabel, nLabel;
     private JTextField nField;
     private JButton calcFiboButton, backButton, closeButton;
+    private JScrollPane scrollResult;
+    private JEditorPane resultLabel;
 
     private int n; // Variable a contar
+
+    private String result;
 
     private JFrame backFrame; // Manejo de la ventana anterior
     private JSeparator underlineTitle;
@@ -49,11 +53,25 @@ public class FiboGUI extends JFrame implements ActionListener {
         calcFiboButton.addActionListener(this);
         add(calcFiboButton);
 
-        resultLabel = new JLabel("Aquí debería aparecer el resultado");
-        resultLabel.setVerticalAlignment(resultLabel.TOP);
-        resultLabel.setHorizontalAlignment(resultLabel.CENTER);
+        resultLabel = new JEditorPane();
+        resultLabel.setContentType("text/html");
+        resultLabel.setBackground(Styles.boneWhite);
+        resultLabel.setText("<html><div style='font-size: 15px; font-family: \"Product Sans\"," +
+                " Roboto; text-align: center;'>" +
+                "Aquí debería aparecer el resultado" +
+                "</div></html>");
         resultLabel.setBounds(10, 200, 400, 210);
         add(resultLabel);
+
+
+        scrollResult = new JScrollPane(resultLabel);
+        scrollResult.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        ChatUtilities.CustomScrollBarUI customScrollBarUI = new ChatUtilities.CustomScrollBarUI();
+        scrollResult.getVerticalScrollBar().setUI(customScrollBarUI);
+        scrollResult.setBounds(10, 200, 400, 210);
+        add(scrollResult);
+
+
 
         backButton = new JButton("Volver");
         backButton.setBounds(10, 415, 195, 45);
@@ -76,21 +94,24 @@ public class FiboGUI extends JFrame implements ActionListener {
         // Calculo y muestreo de la serie de Fibonacci
         if (event.getSource() == calcFiboButton || event.getSource() == nField) {
             try {
-                n = Integer.parseInt(nField.getText()); // Obtener el número
+                n = Integer.parseInt(nField.getText());// Obtener el número"
             } catch (Exception NumberFormatException) {
                 JOptionPane.showMessageDialog(this, "Error\n\nDebe ser un número entero.");
             }
 
             if (n < 0) { // Revisión de número positivo
                 JOptionPane.showMessageDialog(this, "Error\n\nDebe ser un número positivo.");
+            } else if ( n > 93) {
+                JOptionPane.showMessageDialog(this, "Error\n\nNo puede pasar de 93.");
             }
             nField.setText("");
 
             // Devolución del arreglo con los números de la secuencia
+
             switch (n) {
-                case 0 -> resultLabel.setText("Resultado: Ningún número.");
-                case 1 -> resultLabel.setText("Resultado: 0");
-                case 2 -> resultLabel.setText("Resultado: 0, 1");
+                case 0 -> result = "Ningún número";
+                case 1 -> result = "0";
+                case 2 -> result = "0, 1";
                 default -> {
 
                     // se crea un arreglo del tamaño que el usuario decida
@@ -105,13 +126,16 @@ public class FiboGUI extends JFrame implements ActionListener {
                         arreglo[i] = arreglo[i - 1] + arreglo[i - 2];
                     }
 
+                    result = Arrays.toString(arreglo).substring(1, Arrays.toString(arreglo).length() - 1);
 
-                    // impresion de la secuencia fiboonacci
-                    resultLabel.setText("<html>Resultado: " +
-                            Arrays.toString(arreglo).substring(1, Arrays.toString(arreglo).length() - 1) +
-                            "</html>");
                 }
             }
+            // impresión de la secuencia fiboonacci
+            resultLabel.setText(
+                    "<html>" +
+                            "<div style='font-size: 40px; font-family: \"Product Sans\", Roboto; text-align: center;'>" +
+                            result +
+                            "</div></html>");
 
 
         }
